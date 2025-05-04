64fb4000a2b0ac00409b15d54a13f6e875d84a1c
framework-jacobutterworth <jacob.b@weareframework.co.uk>
2025-05-04 12:04:39 +0100

    control ant

--- first.c
+++ first.c
@@ -3,6 +3,14 @@
 
 // --- missing from your toolbox.h: the VCOUNT register 0x04000006
 #define REG_VCOUNT (*(volatile u16 *)(MEM_IO + 0x0006))
+#define REG_KEYINPUT (*(volatile u16 *)0x4000130)
+
+// Key masks
+#define KEY_RIGHT 0x0010
+#define KEY_LEFT 0x0020
+#define KEY_UP 0x0040
+#define KEY_DOWN 0x0080
+#define KEY_MASK 0x03FF
 
 // Direction vectors for the ant (right, up, left, down)
 const int dx[] = {1, 0, -1, 0};
@@ -24,6 +32,12 @@ INLINE u16 m3_get(int x, int y)
   return vid_mem[y * SCREEN_WIDTH + x];
 }
 
+// Function to read key input (returns true if key is pressed)
+INLINE int key_hit(int key)
+{
+  return (~REG_KEYINPUT & key);
+}
+
 int main(void)
 {
   // Mode 3, BG2 on
@@ -32,32 +46,58 @@ int main(void)
   // Starting position in the middle of the screen
   int x = SCREEN_WIDTH / 2;
   int y = SCREEN_HEIGHT / 2;
-  // Start facing upward (index 1 in our direction arrays)
-  int dir = 1;
+  int dir = 1; // Start facing upward
+
+  // Variables to track previous key state
+  int prev_keys = 0;
 
   while (1)
   {
     wait_vblank();
 
-    // Get current cell color
-    u16 current_color = m3_get(x, y);
+    // Get current key state
+    int curr_keys = ~REG_KEYINPUT & KEY_MASK;
+    int new_presses = curr_keys & ~prev_keys;
 
-    if (current_color == CLR_BLACK)
+    // Handle manual movement
+    if (new_presses & KEY_RIGHT)
     {
-      // On black: turn left (add 1 to direction)
-      dir = (dir + 1) % 4;
-      m3_plot(x, y, CLR_WHITE);
+      x++;
+    }
+    else if (new_presses & KEY_LEFT)
+    {
+      x--;
+    }
+    else if (new_presses & KEY_UP)
+    {
+      y--;
+    }
+    else if (new_presses & KEY_DOWN)
+    {
+      y++;
     }
     else
     {
-      // On white/unvisited: turn right (subtract 1 from direction)
-      dir = (dir + 3) % 4; // same as (dir - 1 + 4) % 4
-      m3_plot(x, y, CLR_BLACK);
-    }
+      // No key pressed - do automatic Langton's Ant movement
+      u16 current_color = m3_get(x, y);
 
-    // Move forward in current direction
-    x += dx[dir];
-    y += dy[dir];
+      if (current_color == CLR_BLACK)
+      {
+        // On black: turn left
+        dir = (dir + 1) % 4;
+        m3_plot(x, y, CLR_WHITE);
+      }
+      else
+      {
+        // On white/unvisited: turn right
+        dir = (dir + 3) % 4; // same as (dir - 1 + 4) % 4
+        m3_plot(x, y, CLR_BLACK);
+      }
+
+      // Move forward in current direction
+      x += dx[dir];
+      y += dy[dir];
+    }
 
     // Wrap around screen edges
     if (x < 0)
@@ -68,6 +108,16 @@ int main(void)
       y = SCREEN_HEIGHT - 1;
     if (y >= SCREEN_HEIGHT)
       y = 0;
+
+    // If we moved by key press, flip the color
+    if (new_presses & (KEY_RIGHT | KEY_LEFT | KEY_UP | KEY_DOWN))
+    {
+      u16 current_color = m3_get(x, y);
+      m3_plot(x, y, current_color == CLR_BLACK ? CLR_WHITE : CLR_BLACK);
+    }
+
+    // Store current key state for next frame
+    prev_keys = curr_keys;
   }
 
   return 0;
